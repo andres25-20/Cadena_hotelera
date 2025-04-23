f51f510270676273726a09c7c09901379ac91333
Andres PintoE <afpinto13@gmail.com>
2025-04-23 03:33:28 -0500

    Update api.php
    se realiza actualizacion de las rutas

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,1 +1,24 @@
 <?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HotelController;
+use App\Http\Controllers\HabitacionController;
+use App\Http\Controllers\TipoHabitacionController;
+use App\Http\Controllers\AcomodacionController;
+use App\Http\Controllers\ReglaAcomodacionController;
+use App\Http\Controllers\RolController;
+use App\Http\Controllers\UserController;
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::apiResource('hoteles', HotelController::class);
+Route::apiResource('habitaciones', HabitacionController::class);
+Route::apiResource('tipos-habitacion', TipoHabitacionController::class);
+Route::apiResource('acomodaciones', AcomodacionController::class);
+Route::apiResource('reglas-acomodacion', ReglaAcomodacionController::class);
+Route::apiResource('roles', RolController::class);
+Route::apiResource('usuarios', UserController::class);
