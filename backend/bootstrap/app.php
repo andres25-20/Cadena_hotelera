3791b53ae318a5ae1f2fd3eccc2e1ea697b20a96
Andres PintoE <afpinto13@gmail.com>
2025-04-24 04:40:17 -0500

    Actualizacion consultas sin token
    se registran rutas permitidas para realizar consultas, actualizaciones sin requerir token o login

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -11,7 +11,16 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(['api/usuarios',
+    'api/usuarios/*','api/hoteles',
+    'api/hoteles/*','api/ciudades',
+    'api/ciudades/*','api/habitaciones',
+    'api/habitaciones/*','api/tipos-habitacion',
+    'api/tipos-habitacion/*','api/roles',
+    'api/roles/*','api/reglas-acomodacion',
+    'api/reglas-acomodacion/*','api/reglas-acomodacion/tipo',
+    'api/reglas-acomodacion/tipo/*','api/acomodaciones',
+    'api/acomodaciones/*','api/acomodaciones/permitidas']);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
